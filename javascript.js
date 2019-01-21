1291592f9e24e08ffd5c54e695971bfaddc6183b
crzdieu <bryanwp88@gmail.com>
2019-01-21 12:02:18 +0100

    exercice4

---
+++ javascript.js
@@ -0,0 +1,6 @@
+function toggle() {
+var nom = document.getElementById("lastname").value;
+var prenom = document.getElementById("firstname").value;
+var ville = document.getElementById("city").value;
+alert(nom+"\n"+prenom+"\n"+ville);
+}
